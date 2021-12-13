6ddae15e84b102b2171ea8edea1dd2e6c1b4b038
Aaryaman Maheshwari <67153585+fullstackslayer@users.noreply.github.com>
2021-12-13 10:33:34 +0530

    fix stuff

--- src/Bot.ts
+++ src/Bot.ts
@@ -1,12 +1,16 @@
-import * as DJS from "discord.js";
+import * as Discord from "discord.js";
 import * as dotenv from "dotenv";
 
+import {
+  ABOUT_CHANNEL_ID,
+  COMMAND_PREFIX,
+  INTRODUCTIONS_CHANNEL_ID,
+  RULES,
+} from "./constants";
 import { PresenceData } from "discord.js";
 
-import { IntroductionManager } from "./managers/introduction-manager";
-
 export class Bot {
-  private static client: DJS.Client;
+  private static client: Discord.Client;
 
   // starts the bot
   public static async start() {
@@ -16,9 +20,84 @@ export class Bot {
     console.log("Bot up and running.");
   }
 
-  // load the modules for the bot
-  private static async loadManagers() {
-    new IntroductionManager(Bot.client);
+  // load the command modules for the bot
+  private static async loadCommandModules() {
+    this.client.on("message", async (message) => {
+      // embeds
+      async function rulesEmbeds() {
+        if (
+          message.content === `${COMMAND_PREFIX}rules` &&
+          message.member.permissions.has(["ADMINISTRATOR"])
+        ) {
+          const socialRow = new Discord.MessageActionRow().addComponents(
+            new Discord.MessageButton()
+              .setLabel("GitHub")
+              .setStyle("LINK")
+              .setURL("https://github.com/buildergroop")
+              .setEmoji("👨‍💻"),
+            new Discord.MessageButton()
+              .setLabel("Twitter")
+              .setStyle("LINK")
+              .setURL("https://twitter.com/buildergroop")
+              .setEmoji("🐦"),
+            new Discord.MessageButton()
+              .setLabel("Website")
+              .setStyle("LINK")
+              .setURL("https://buildergroop.com")
+              .setEmoji("🌐")
+          );
+
+          await message.channel.send(
+            "https://images-ext-2.discordapp.net/external/HVfpoZA4O_9xB5YO2lxtt1cr81xnxGgKdVZEA_S9eEM/%3Fwidth%3D1694%26height%3D430/https/media.discordapp.net/attachments/913702607510466651/913877293619904613/Rules_And_Info.png?width=1484&height=377"
+          );
+          await message.channel.send({
+            embeds: [
+              {
+                color: "#0099ff",
+                title: "Welcome To Buildergroop",
+                description:
+                  "We're a community full of ambitious young builders, striving to make the world a better place through innovation.",
+              },
+            ],
+            components: [socialRow],
+          });
+          await message.channel.send({
+            embeds: [
+              {
+                color: "#535061",
+                title: "Our Community Rules",
+                description: RULES,
+              },
+            ],
+          });
+          await message.channel.send({
+            embeds: [
+              {
+                color: "#535061",
+                title: "What to do after joining",
+                description: `
+      Firstly, We highly recommend choosing some awesome roles over in <#913709531442315324>.
+      
+      Next, you should go ahead and introduce yourself over in <#${INTRODUCTIONS_CHANNEL_ID}>. By introducing yourself, you get access to exclusive buildergroop events and perks!
+
+      Go to <#${ABOUT_CHANNEL_ID}> to learn more about the server.
+                    `,
+              },
+            ],
+          });
+        }
+      }
+
+      rulesEmbeds();
+
+      // handlers
+      async function handleIntroduction() {
+        if (message.channelId === INTRODUCTIONS_CHANNEL_ID) {
+          await message.member?.roles.add("913766127451136002");
+        }
+      }
+      handleIntroduction();
+    });
   }
 
   // set the bot presence
@@ -33,25 +112,23 @@ export class Bot {
       ],
     };
 
-    await Bot.client.user?.setPresence(presence);
+    Bot.client.user?.setPresence(presence);
   }
 
   private static async createClient() {
     // set the client
-    Bot.client = new DJS.Client({
+    Bot.client = new Discord.Client({
       intents: [
-        "GUILDS",
-        "GUILD_MESSAGES",
-        "GUILD_MESSAGE_REACTIONS",
-        "DIRECT_MESSAGES",
-        "DIRECT_MESSAGE_REACTIONS",
+        Discord.Intents.FLAGS.GUILDS,
+        Discord.Intents.FLAGS.GUILD_MESSAGES,
+        Discord.Intents.FLAGS.DIRECT_MESSAGES,
       ],
       partials: ["CHANNEL", "MESSAGE", "REACTION", "USER", "GUILD_MEMBER"],
     });
 
     // when the client is created
     Bot.client.on("ready", async () => {
-      await this.loadManagers();
+      await this.loadCommandModules();
       await this.setPresence();
     });
 
--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,25 @@
 export const COMMAND_PREFIX: string = "?";
 export const ENABLE_UNKOWN_COMMAND_MESSAGE: boolean = true;
+
 export const INTRODUCTIONS_CHANNEL_ID: string = "913701412578418718";
+export const INFORMATION_CHANNEL_ID: string = "913669662649237564";
+export const ABOUT_CHANNEL_ID: string = "913729308676202506";
+export const ROLES_CHANNEL_ID: string = "913709531442315324";
+
+export const RULES: string = `
+Please read the following rules prior to interacting in the server.
+
+• Keep all messages and content SFW
+• Harassment and Hate Speech is prohibited
+• Do not share pirated/crack software and content
+• Discussing sensitive topics (civics, religion, health) or is frowned upon
+• No Spamming
+• No Doxxing
+• Send malicious links or viruses is prohibited
+• Refrain from using any language except English
+• Asking moderators for leniency in punishment is strictly disallowed
+• Use common sense
+• Follow Discord’s Terms of Service.
+
+The rules mentioned here only exist for the safety of members like you. They are subject to change at any time. Not being aware of the present list of rules is not a valid excuse to be protected from consequences.
+`;
