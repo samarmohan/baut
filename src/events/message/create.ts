a8f18ba8b2d9bf42d7af764a096c111cd29c9273
Matthew Adcock <matthew.n.adcock@gmail.com>
2021-12-17 15:04:09 +0000

    Handle user elegibility

---
+++ src/events/message/create.ts
@@ -0,0 +1,29 @@
+import { GuildMember } from 'discord.js';
+import Event from '../../structures/Event';
+
+export default new Event({
+	name: 'messageCreate',
+}, async (client, message) => {
+	// Ignore bots
+	if (message.author.bot) return;
+
+	// Destructure the constants
+	const { roles, channels } = client.constants;
+
+	// Introductions channel only
+	message.channel.id === channels.intros && setElegible(message.member);
+
+	// Handle user eligibility
+	async function setElegible(member: GuildMember) {
+		// Get the roles
+		const notEligibleRole = member.guild.roles.cache.get(roles.not_eligible);
+		const eligibleRole = member.guild.roles.cache.get(roles.eligible);
+
+		// Check if the member is eligible already
+		if (member.roles.cache.has(roles.eligible)) return;
+
+		// Add the eligible role and remove the not eligible role
+		await member.roles.remove(notEligibleRole);
+		await member.roles.add(eligibleRole);
+}
+});
