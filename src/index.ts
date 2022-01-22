93ccc93f2a30e88186b3a7fbd51205c2a8ad21e8
Bereket Semagn <semagnbereket@gmail.com>
2022-01-22 12:39:50 -0500

    feat: guildMemberAdd event

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import { Mammot } from "@mammot/core";
 import { RulesCommand, PingCommand, IntroCommand, RolesCommand } from "./cmds";
 import { clientOptions } from "./config";
-import 'dotenv/config';
+import "dotenv/config";
 
 export const mammot = Mammot.client({
   ...clientOptions,
+});
+
+mammot.client.on("guildMemberAdd", (member) => {
+  member.send("Welcome to Buildergroop!");
 });
 
 mammot
