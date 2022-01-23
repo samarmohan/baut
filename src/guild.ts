79c56cc3eb89072cf6b366f93ae1d74c24e6fe53
Aaryaman Maheshwari <hey@aaryaman.io>
2022-01-23 18:00:42 +0530

    fix import error

--- src/guild.ts
+++ src/guild.ts
@@ -3,7 +3,7 @@ This file houses information such as ID's of specific entities (e.g roles, chann
 */
 
 import { EmbedMessages } from "./types";
-import { Roles } from "./types/rolesProd";
+import { Roles } from "./types/RolesProd";
 
 export const roles: Roles = {
   // Career
