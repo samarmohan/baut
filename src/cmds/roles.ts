060a8713a30113a74b78195ebda7856281692a1d
Polar <89708970+polargh@users.noreply.github.com>
2022-01-29 03:49:52 -0600

    Revert "finish later"
    This reverts commit 174abb94be5c389944daca4a3a0884d8cb23da63.

--- src/cmds/roles.ts
+++ src/cmds/roles.ts
@@ -1,112 +1,87 @@
 import {
-	MessageActionRow,
-	MessageButton,
-	CommandInteraction,
-	MessageAttachment,
-	Collector,
-	Message,
+  MessageActionRow,
+  MessageButton,
+  CommandInteraction,
+  MessageAttachment,
 } from "discord.js";
 import { config, Command } from "@mammot/core";
 import { verifyAdmin } from "../util/verifyAdmin";
 
 @config("roles", {
-	description: "Select your self-assigned roles (Admin Only Command)",
-	inhibitors: [verifyAdmin()],
+  description: "Select your self-assigned roles (Admin Only Command)",
+  inhibitors: [verifyAdmin()],
 })
 export class RolesCommand extends Command {
-	public async run(interaction: CommandInteraction) {
-		const rolesChannel = "929317788659621889";
+  public async run(interaction: CommandInteraction) {
+    const rolesChannel = "934094517525676042";
+    const rulesChannel = "913669662649237564";
 
-		if (interaction.channel.id !== rolesChannel) {
-			// Direct users to the rules channel
-			return await interaction.reply({
-				content: `Please go to <#${rolesChannel}> to send the server roles.`,
-				ephemeral: true,
-			});
-		}
+    if (interaction.channel.id !== rolesChannel) {
+      // Direct users to the rules channel
+      return await interaction.reply({
+        content: `Please go to <#${rulesChannel}> to view the server rules.`,
+        ephemeral: true,
+      });
+    }
 
-		// Invisible divider
-		const divider = `
+    // Invisible divider
+    const divider = `
 __
 __`;
 
-		// Header image
-		const headerImage = new MessageAttachment(
-			"https://cdn.discordapp.com/attachments/864826842707132446/931217059432525894/Roles_Poster.png"
-		);
+    // Header image
+    const headerImage = new MessageAttachment(
+      "https://cdn.discordapp.com/attachments/864826842707132446/931217059432525894/Roles_Poster.png"
+    );
 
-		// Create category buttons
-		const categoryButtons = new MessageActionRow().addComponents(
-			new MessageButton()
-				.setLabel("Career")
-				.setStyle("PRIMARY")
-				.setEmoji("🧑‍💼")
-				.setCustomId("careers"),
-			new MessageButton()
-				.setLabel("Location")
-				.setStyle("PRIMARY")
-				.setEmoji("✈️")
-				.setCustomId("location"),
-			new MessageButton()
-				.setLabel("Pronouns")
-				.setStyle("PRIMARY")
-				.setEmoji("💁")
-				.setCustomId("pronouns"),
-			new MessageButton()
-				.setLabel("Experience")
-				.setStyle("PRIMARY")
-				.setEmoji("📊")
-				.setCustomId("experience"),
-			new MessageButton()
-				.setLabel("Notifications")
-				.setStyle("PRIMARY")
-				.setEmoji("🔔")
-				.setCustomId("notifications")
-		);
+    // Create category buttons
+    const categoryButtons = new MessageActionRow().addComponents(
+      new MessageButton()
+        .setLabel("Career")
+        .setStyle("PRIMARY")
+        .setEmoji("🧑‍💼")
+        .setCustomId("careers"),
+      new MessageButton()
+        .setLabel("Location")
+        .setStyle("PRIMARY")
+        .setEmoji("✈️")
+        .setCustomId("location"),
+      new MessageButton()
+        .setLabel("Pronouns")
+        .setStyle("PRIMARY")
+        .setEmoji("💁")
+        .setCustomId("pronouns"),
+      new MessageButton()
+        .setLabel("Experience")
+        .setStyle("PRIMARY")
+        .setEmoji("📊")
+        .setCustomId("experience"),
+      new MessageButton()
+        .setLabel("Notifications")
+        .setStyle("PRIMARY")
+        .setEmoji("🔔")
+        .setCustomId("notifications")
+    );
 
-		// Send career embed and select menu
-		await interaction.channel.send({
-			files: [headerImage],
-		});
+    // Send career embed and select menu
+    await interaction.channel.send({
+      files: [headerImage],
+    });
 
-		// Check button if was clicked if clicked return text
+    // send invisible divider
+    await interaction.channel.send({
+      content: divider,
+    });
 
-		const collector = interaction.channel.createMessageComponentCollector({ time: 15000 });
-		collector.on("collect", async i => {
-			if (i.customId === "notifications") {
-				await i.deferUpdate();
-				await i.followUp({ content: "notifications", components: [], ephemeral: true });
-			} else if (i.customId === "location") {
-				await i.deferUpdate();
-				await i.followUp({ content: "location", components: [], ephemeral: true });
-			} else if (i.customId === "pronouns") {
-				await i.deferUpdate();
-				await i.followUp({ content: "pronouns", components: [], ephemeral: true });
-			} else if (i.customId === "experience") {
-				await i.deferUpdate();
-				await i.followUp({ content: "experience", components: [], ephemeral: true });
-			} else if (i.customId === "careers") {
-				await i.deferUpdate();
-				await i.followUp({ content: "careers", components: [], ephemeral: true });
-			} else {
-				return;
-			}
-		});
+    // Send the category buttons
+    await interaction.channel.send({
+      content: `**Please select a category to view the available roles.** _ _`,
+      components: [categoryButtons],
+    });
 
-		// send invisible divider
-		await interaction.channel.send({
-			content: divider,
-		});
-
-		// Send the category buttons
-		await interaction.channel.send({
-			content: `**Please select a category to view the available roles.** _ _`,
-			components: [categoryButtons],
-		});
-
-		await interaction.reply({
-			content: "Sent roles message",
-			ephemeral: true,
-		});
-	}
+    await interaction.reply({
+      content: "Sent roles message",
+      ephemeral: true,
+    });
+  }
 }
--- src/util/fileLoader.ts
+++ src/util/fileLoader.ts
@@ -11,17 +11,17 @@ import Event from "../structures/Event";
  * @param {string} dir The root dir
  */
 export default async function* fileloader(dir: string) {
-	const files = await readdir(dir, { withFileTypes: true });
+  const files = await readdir(dir, { withFileTypes: true });
 
-	for (const file of files) {
-		const res: PathLike = resolve(dir, file.name);
+  for (const file of files) {
+    const res: PathLike = resolve(dir, file.name);
 
-		if (file.isDirectory()) {
-			yield* fileloader(res);
-		} else {
-			yield res;
-		}
-	}
+    if (file.isDirectory()) {
+      yield* fileloader(res);
+    } else {
+      yield res;
+    }
+  }
 }
 
 /**
@@ -30,20 +30,24 @@ export default async function* fileloader(dir: string) {
  * @param {string} dir The dir containing the event files
  */
 export async function loadEvents(mammot: Mammot, dir: string) {
-	const files = fileloader(dir);
+  const files = fileloader(dir);
 
-	for await (const file of files) {
-		const importedFile = await import(file);
-		const event = importedFile.default || importedFile;
+  for await (const file of files) {
+    const importedFile = await import(file);
+    const event = importedFile.default || importedFile;
 
-		if (event.once) {
-			mammot.client.once(event.name, (...args) => event.execute(mammot.client, ...args));
-		} else {
-			mammot.client.on(event.name, (...args) => event.execute(event.client, ...args));
-		}
+    if (event.once) {
+      mammot.client.once(event.name, (...args) =>
+        event.execute(mammot.client, ...args)
+      );
+    } else {
+      mammot.client.on(event.name, (...args) =>
+        event.execute(event.client, ...args)
+      );
+    }
 
-		console.log(`Loaded event ${event.name}`);
-	}
+    console.log(`Loaded event ${event.name}`);
+  }
 }
 
 // /**
@@ -51,21 +55,26 @@ export async function loadEvents(mammot: Mammot, dir: string) {
 //  * @param {Client} client The Discord client
 //  * @param {string} dir The root components directory
 //  */
-// export async function loadComponents(mammot: Mammot, dir: string): Promise<void> {
-// 	const files = fileloader(dir);
+// export async function loadComponents(
+//   client: Client,
+//   dir: string
+// ): Promise<void> {
+//   const files = fileloader(dir);
 
-// 	for await (const file of files) {
-// 		const importedFile = await import(file);
-// 		const component = importedFile.default || importedFile;
+//   for await (const file of files) {
+//     const importedFile = await import(file);
+//     const component = importedFile.default || importedFile;
 
-// 		if (!(component instanceof Component)) continue;
+//     if (!(component instanceof Component)) continue;
 
-// 		if (!component.id) {
-// 			console.error(`Failed to load component ${basename(file)} as it has no id`);
-// 			continue;
-// 		}
+//     if (!component.id) {
+//       console.error(
+//         `Failed to load component ${basename(file)} as it has no id`
+//       );
+//       continue;
+//     }
 
-// 		mammot.client.channels.set(component.id, component);
-// 		console.log(`Loaded component ${component.id}`);
-// 	}
+//     client.components.set(component.id, component);
+//     console.log(`Loaded component ${component.id}`);
+//   }
 // }
