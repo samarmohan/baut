3b50f82d3bae2f62d74f0890900ad72d62391905
tnb24 <tanaybhardwaj24@gmail.com>
2022-02-03 06:01:09 +0000

    silly err fix

--- src/util/embeds.ts
+++ src/util/embeds.ts
@@ -4,28 +4,28 @@ export const careerEmbed = new MessageEmbed()
 	.setTitle('What do you do?')
 	.setDescription('Choose what best defines your career.')
 	.setColor('#535061')
-	.setFooter('Multi Select');
+	.setFooter({text: 'Multi Select'});
 
 export const experienceEmbed = new MessageEmbed()
 	.setTitle('Choose your experience level')
 	.setDescription('How much experience do you have in your current career(s)/hobby(s) (i.e Developer, Designer, Entrepreneur, Creator)?')
 	.setColor('#535061')
-	.setFooter('Single Select');
+	.setFooter({text: 'Single Select'});
 
 export const locationEmbed = new MessageEmbed()
 	.setTitle('Where are you located?')
 	.setDescription('Choose which continent you live in.')
 	.setColor('#535061')
-	.setFooter('Single Select');
+	.setFooter({text: 'Single Select'});
 
 export const notificationEmbed = new MessageEmbed()
 	.setTitle('Select Ping Roles')
 	.setDescription('Choose which of the following you would like to be pinged for')
 	.setColor('#535061')
-	.setFooter('Multi Select');
+	.setFooter({text: 'Multi Select'});
 
 export const pronounsEmbed = new MessageEmbed()
 	.setTitle('What are your pronouns?')
 	.setDescription('Select your pronouns (Skip if you\'d rather not disclose).')
 	.setColor('#535061')
-	.setFooter('Single / Multi Select');
+	.setFooter({text: 'Single / Multi Select'});
