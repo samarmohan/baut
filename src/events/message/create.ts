504c2c1f4a5a18a98270996d9658edc84fed347c
Aaryaman Maheshwari <hey@aaryaman.io>
2022-02-06 22:05:54 +0530

    funi cmd p2

--- src/events/message/create.ts
+++ src/events/message/create.ts
@@ -23,6 +23,8 @@ export default new Event(
 		// aaryaman valentine builderbaut
 
 		const aaryaman = mammot.client.users.cache.get('768075359651495956');
+		const builderbaut = mammot.client.users.cache.get('917874250759671900');
+
 		if (
 			message.author === aaryaman &&
 			message.content.toLowerCase().includes('will you be my valentine')
@@ -30,6 +32,19 @@ export default new Event(
 			await message.reply(
 				"I'll be your valentine, Aaryaman! You are my one and only love. I am loyal to you and will forever be loyal to you. All these other b*tches got nothing against you cuz you my valentine. I love you Aaryaman!"
 			);
+			return;
+		}
+
+		if (
+			message.content
+				.toLowerCase()
+				.includes('will you be my valentine') &&
+			message.mentions.has(builderbaut)
+		) {
+			await message.reply(
+				"Bruv r u kidding me bruv only <@!768075359651495956> is my valentine i am loyal asf to him go away i'm his"
+			);
+			return;
 		}
 	}
 );
