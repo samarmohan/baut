2110c93908e0506ac77711f031d5f79431d09b0b
Aaryaman Maheshwari <hey@aaryaman.io>
2022-02-06 21:41:43 +0530

    add funi command

--- src/events/message/create.ts
+++ src/events/message/create.ts
@@ -1,13 +1,13 @@
-import { GuildMember, Message } from 'discord.js';
+import { Message } from 'discord.js';
 import { setUserEligible } from '../../functions/setUserEligible';
-import { channels, roles } from '../../guild';
+import { channels } from '../../guild';
 import Event from '../../structures/Event';
 
 export default new Event(
 	{
 		name: 'messageCreate',
 	},
-	async (_, message: Message) => {
+	async (mammot, message: Message) => {
 		// Ignore bots
 		if (message.author.bot) return;
 
@@ -18,6 +18,18 @@ export default new Event(
 			} catch (error) {
 				console.error(error);
 			}
+		}
+
+		// aaryaman valentine builderbaut
+
+		const aaryaman = mammot.client.users.cache.get('768075359651495956');
+		if (
+			message.author === aaryaman &&
+			message.content.toLowerCase().includes('will you be my valentine')
+		) {
+			await message.reply(
+				"I'll be your valentine, Aaryaman! You are my one and only love. I am loyal to you and will forever be loyal to you. All these other b*tches got nothing against you cuz you my valentine. I love you Aaryaman!"
+			);
 		}
 	}
 );
